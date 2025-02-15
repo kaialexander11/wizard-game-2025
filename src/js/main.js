d24df30bf64a5a6358631a7ddda98b733c69c55f
Alexander Uzunov <kaialexander.11@gmail.com>
2025-02-15 17:24:05 +0200

    Add game engine

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,12 @@ let game = initGameObject();
 
 game.startScreen.addEventListener('click', (e) => {
 
-    console.log('Start game!');
+
+    //console.log('Start game!');
     game.startScreen.classList.add('hidden');
     game.gameScreen.classList.remove('hidden');
 
-    
+    // Start game
+    start(state, game);
 
 });
