522e9d8fefea90cf974d3d5a9a5b4101aac8aad8
Alexander Uzunov <kaialexander.11@gmail.com>
2025-02-16 08:29:08 +0200

    Add right movement plus some corrections

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -9,8 +9,20 @@ function start(state, game) {
 
 function gameLoop(state, game) {
 
+    const { wizard } = state;
+    const { wizardElement } = game;
+
     //console.log(state.player);
-    console.log(state.keys);
+    //console.log(state.keys);
+
+    // Move wizard:
+    if (state.keys.KeyD) {
+        //console.log(state.keys); => works!
+        wizard.posX += 10;
+    }
+
+    // Render
+    wizardElement.style.left = wizard.posX + 'px';
 
     window.requestAnimationFrame(gameLoop.bind(null, state, game));
 
--- src/js/gameObjects.js
+++ src/js/gameObjects.js
@@ -16,16 +16,17 @@ function initGameObject() {
             let wizardElement = document.createElement('div');
             wizardElement.classList.add('wizard');
 
-            wizardElement.style.height = initialState.height + 'px';
             wizardElement.style.width = initialState.width + 'px';
+            wizardElement.style.height = initialState.height + 'px';
 
-            wizardElement.style.left = initialState.startX + 'px';
-            wizardElement.style.top = initialState.startY + 'px';
+            wizardElement.style.left = initialState.posX + 'px';
+            wizardElement.style.top = initialState.posY + 'px';
 
             this.wizardElement = wizardElement;
+
             gameScreen.appendChild(wizardElement);
-            
-            return wizard;
+
+            return wizardElement;
 
         }
         
